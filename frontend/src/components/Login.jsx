2efa2a9ee876b9aefb16c7327899b43d09bf3f22
ZackCornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-03-18 13:49:29 +0800

    .

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
         const decodedToken = jwtDecode(token);
         const userId = decodedToken.id;
         localStorage.setItem('userId', userId);
-        window.location.href = '/feed'; // Redirect to home page
+        window.location.href = '/posts/feed'; // Redirect to home page
       }
     } catch (error) {
       console.error('Login failed', error);
